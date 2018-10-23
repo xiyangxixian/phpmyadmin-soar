b58314c0185ae4567451e82244b86ff1de919729
2554758802@qq.com <>
2018-10-23 20:11:51 +0800

    add phpmyadmin version

--- soar.php
+++ soar.php
@@ -1,47 +1,57 @@
 <?php
 class Soar {
-	
-	private $config = [
-		'report-type' => 'json',
-		'allow-online-as-test' => 'true'
-	];
-	private $cmd = '';
-	
-	public function __construct($config = []){
-        $this->config($config);
-		$this->cmd = DIRECTORY_SEPARATOR == '\\' ? __DIR__ . '/bin/soar.windows-amd64.exe' : __DIR__ . '/bin/soar.linux-amd64';
-		$this->cmd .= ' ' . $this->buildConfig();
-	}
-    
-    public function config($config){
-		$this->config = array_merge($this->config, $config);
-    }
 
-    private function buildConfig(){
-		$options = [];
-		foreach ($this->config as $key=>$val) {
-			$options[] = "-{$key}={$val}";
-		}
-		return implode(' ', $options);
-	}
-	
-	public function analysis($sql){
-        $sql = trim(preg_replace('/^explain/i', '', $sql));
-		$f=proc_open($this->cmd,[
-			['pipe', 'r'],['pipe','w'],['pipe','w']
-		 ],$pipes);
-		fwrite($pipes[0], $sql);
-		fclose($pipes[0]);
-        
-        if (DIRECTORY_SEPARATOR == '\\') fgets($pipes[1]);
-		$data = stream_get_contents($pipes[1]);
-		fclose($pipes[1]);
-		return $this->config['report-type'] == 'json' ? json_decode($data, true) : $data;
-	}
+  private $config = [
+    'report-type' => 'json',
+    'allow-online-as-test' => 'true'
+  ];
+  private $cmd = '';
+
+  public function __construct($config = []) {
+    $this->config($config);
+    $this->cmd = $this->getCmd();
+    $this->cmd .= ' ' . $this->buildConfig();
+  }
+
+  private function getCmd() {
+    defined('PHP_OS') or define('PHP_OS', 'Linux');
+    if (DIRECTORY_SEPARATOR == '\\') {
+      $cmd = 'soar.windows-amd64.exe';
+    } else {
+      $cmd = stristr(PHP_OS, 'darwin') ? 'soar.darwin-amd64' : 'soar.linux-amd64';
+    }
+    return __DIR__ . '/bin/' . $cmd;
+  }
+
+  public function config($config) {
+    $this->config = array_merge($this->config, $config);
+  }
+
+  private function buildConfig() {
+    $options = [];
+    foreach ($this->config as $key => $val) {
+      $options[] = "-{$key}={$val}";
+    }
+    return implode(' ', $options);
+  }
+
+  public function analysis($sql) {
+    $sql = trim(preg_replace('/^explain/i', '', $sql));
+    $f = proc_open($this->cmd, [
+      ['pipe', 'r'], ['pipe', 'w'], ['pipe', 'w']
+    ], $pipes);
+    fwrite($pipes[0], $sql);
+    fclose($pipes[0]);
+    
+    if (DIRECTORY_SEPARATOR == '\\') fgets($pipes[1]);
+    $data = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    return $this->config['report-type'] == 'json' ? (json_decode($data, true) ?: [[]]) : $data;
+  }
 }
 
 class SoarHtml {
-  
+
   private $r;
   
   private $config;
@@ -54,8 +64,8 @@ class SoarHtml {
     $this->r = $arr;
     $this->parseResult();
   }
-  
-  private function parseResult(){
+
+  private function parseResult() {
     $total = 100;
     $explainItem = [];
     $analysis = [];
@@ -69,7 +79,7 @@ class SoarHtml {
         $analysis[] = $val;
       }
     }
-    usort($analysis, function ($a, $b){
+    usort($analysis, function ($a, $b) {
       return $b['Level'] - $a['Level'];
     });
     $this->config['num'] = $total < 0 ? 0 : $total;
@@ -78,7 +88,27 @@ class SoarHtml {
   }
 
   public function asNumHtml() {
-    return "<h3 style=\"margin:20px 0px 0px 0px;\">评分：{$this->config['num']}分</h3>";
+    $margin = '20px 0px 00px 0px';
+    $verArr = explode('.', PMA_VERSION);
+    $ver = "{$verArr[0]}.{$verArr[1]}";
+    switch ($ver) {
+      case '4.8';
+        $margin = '20px 0px 10px 0px';
+        break;
+      case '4.7';
+      case '4.6';
+        $margin = '20px 0px 0px 0px';
+        break;
+      case '4.5';
+      case '4.4';
+      case '4.3';
+      case '4.2';
+      case '4.1';
+      case '4.0';
+        $margin = '0px 0px 0px 0px';
+        break;
+    }
+    return "<h3 style=\"margin:{$margin}\">评分：{$this->config['num']}分</h3>";
   }
 
   public function asExplainHtml() {
@@ -95,17 +125,17 @@ class SoarHtml {
   }
 
   public function asItemHtml() {
-    $html = ''; 
+    $html = '';
     if ($this->config['analysis']) {
       $html .= '<h3 style="margin:10px 0px;">SQL建议与优化：</h3>';
       $html .= '<table class="table_results ajax pma_table" data-uniqueid="18066"><thead><tr>';
       foreach ($this->columns as $column) {
-        $html .= '<th class="draggable"><span>'.$column.'</span></th>';
+        $html .= '<th class="draggable"><span>' . $column . '</span></th>';
       }
       $html .= '<td class="print_ignore"><span></span></td></tr></thead><tbody>';
       foreach ($this->config['analysis'] as $index => $item) {
         $class = ($index % 2) ? 'even' : 'odd';
-        $html .= '<tr class="'.$class.'">';
+        $html .= '<tr class="' . $class . '">';
         foreach ($this->columns as $column) {
           $html .= "<td data-decimals=\"0\" data-type=\"string\" class=\"data text\"><span>{$item[$column]}</span></td>";
         }
@@ -118,21 +148,33 @@ class SoarHtml {
 }
 
 if (defined('PMA_VERSION')) {
-    $db = $GLOBALS['db'];
-    global $cfg;
-    $host = $cfg['Server']['host'];
-    $user = $cfg['Server']['user'];
-    $pwd = $cfg['Server']['password'];
-    $port = $cfg['Server']['port'] ?: '3306';
+  $db = $GLOBALS['db'];
+  global $cfg;
+  $host = $cfg['Server']['host'];
+  $user = $cfg['Server']['user'];
+  $pwd = $cfg['Server']['password'];
+  $port = $cfg['Server']['port'] ?: '3306';
+  if (preg_match('/[@:\/]/', $pwd)) {
+    $file = __DIR__. '/bin/soar.yaml';
+    $content = "test-dsn:\n  addr: '{$host}:{$port}'\n  schema: '{$db}'\n  user: '{$user}'\n  password: '{$pwd}'\n  disable: false";
+    file_put_contents($file, $content);
+    $soar = new Soar();
+  } else {
     $dsn = "{$user}:{$pwd}@{$host}:{$port}/$db";
     $soar = new Soar(['test-dsn' => $dsn]);
-    $GLOBALS['soar'] = $soar;
-    
-    function get_analyzed_sql($sqlParser){
-      $sql = '';
-      foreach ($sqlParser->tokens as $val) {
-        if (!is_null($val)) $sql .= $val->token;
-      }
-      return $sql;
+  }
+  $GLOBALS['soar'] = $soar;
+
+  function get_analyzed_sql($sqlParser) {
+    $sql = '';
+    foreach ($sqlParser->tokens as $val) {
+      if (!is_null($val)) $sql .= $val->token;
     }
+    return $sql;
+  }
 }
+
+
+$soar = new Soar(['test-dsn' => 'root:@127.0.0.1/xcx_set']);
+$r = $soar->analysis("select * from `sys_config`");
+var_dump($r);
